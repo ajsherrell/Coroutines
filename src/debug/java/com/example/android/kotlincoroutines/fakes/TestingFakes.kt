d52ddd7a7252982ca31b2173a526e6c9da0a13ef
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-29 15:45:17 -0400

    refactor to suspend functions

--- src/debug/java/com/example/android/kotlincoroutines/fakes/TestingFakes.kt
+++ src/debug/java/com/example/android/kotlincoroutines/fakes/TestingFakes.kt
@@ -45,7 +45,7 @@ class TitleDaoFake(initialTitle: String) : TitleDao {
      */
     private val insertedForNext = Channel<Title>(capacity = Channel.BUFFERED)
 
-    override fun insertTitle(title: Title) {
+    override suspend fun insertTitle(title: Title) {
         insertedForNext.offer(title)
         _titleLiveData.value = title
     }
@@ -71,7 +71,7 @@ class TitleDaoFake(initialTitle: String) : TitleDao {
      * @param unit timeunit
      * @return the next value that was inserted into this dao, or null if none found
      */
-    fun nextInsertedOrNull(timeout: Long = 2_000): String? {
+    suspend fun nextInsertedOrNull(timeout: Long = 2_000): String? {
         var result: String? = null
         runBlocking {
             // wait for the next insertion to complete
@@ -91,7 +91,7 @@ class TitleDaoFake(initialTitle: String) : TitleDao {
  * Testing Fake implementation of MainNetwork
  */
 class MainNetworkFake(var result: String) : MainNetwork {
-    override fun fetchNextTitle() = MakeCompilerHappyForStarterCode() // TODO: replace with `result`
+    override suspend fun fetchNextTitle() = result // TODO: replace with `result`
 }
 
 /**
@@ -100,7 +100,7 @@ class MainNetworkFake(var result: String) : MainNetwork {
 class MainNetworkCompletableFake() : MainNetwork {
     private var completable = CompletableDeferred<String>()
 
-    override fun fetchNextTitle() = MakeCompilerHappyForStarterCode() // TODO: replace with `completable.await()`
+    override suspend fun fetchNextTitle() = completable.await() // TODO: replace with `completable.await()`
 
     fun sendCompletionToAllCurrentRequests(result: String) {
         completable.complete(result)
